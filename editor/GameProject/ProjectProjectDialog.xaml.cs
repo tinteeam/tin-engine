5ad32697105bf361b3ab1fb89ade257e1f499920
randomusert <127417963+randomusert@users.noreply.github.com>
2024-07-30 17:25:41 +0300

    Add project files.

---
+++ editor/GameProject/ProjectProjectDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace editor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectProjectDialog.xaml
+    /// </summary>
+    public partial class ProjectProjectDialog : Window
+    {
+        public ProjectProjectDialog()
+        {
+            InitializeComponent();
+        }
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender == openProject)
+            {
+                if (newProject.IsChecked == true)
+                {
+                    newProject.IsChecked = false;
+                    browserContent.Margin = new Thickness(0);
+                }
+                openProject.IsChecked = true;
+            }
+            else{
+                if (openProject.IsChecked == true)
+                {
+                    openProject.IsChecked = false;
+                    browserContent.Margin = new Thickness(-800,0,0,0);
+                }
+                newProject.IsChecked = true;
+            }
+
+        }
+    }
+}
---
+++ editor/README.md
@@ -0,0 +1,13 @@
+# editor
+you can modify the editor source as it is made in WPF
+
+
+# engine
+made in C++
+
+
+
+# note
+If you don't know what you are doing in the engine source
+
+you should leave it alone
